70b235a106750685d3077cf79e0c94c826abf776
Chau Tran <ctch5@mail.umsl.edu>
2020-05-12 19:06:52 -0500

    remove unused code on client

--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -1,13 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
